b78619376b5be3dc162267a556cb35658cd4460e
Conner Nilsen <conner@launchbyte.io>
2019-12-12 17:09:21 -0800

    Add response

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/message', (req, res) => {
-  console.log("Request received");
+  console.log("Request received" + req);
+  res.send("Success");
 });
 
 app.listen(process.env.PORT, () => { console.log("Server started") });
