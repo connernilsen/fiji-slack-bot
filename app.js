b5503b9a5b1c5f041fc6d6993ccc8cf7412aaa82
Conner Nilsen <conner@launchbyte.io>
2019-12-12 17:52:17 -0800

    Add slash commands

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+import zalgo from "zalgo-js";
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -11,5 +12,10 @@ app.post('/', (req, res) => {
   console.log(req.body);
   res.send(req.body["challenge"]);
 });
+
+app.post('/corrput' (req, res) => {
+  console.log(zalgo(req.body));
+  res.send(zalgo(req.body));
+}
 
 app.listen(process.env.PORT, () => { console.log("Server started") });
