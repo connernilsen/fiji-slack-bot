e310d75c9b99c0061a7213410f2357347e0fce4b
Conner Nilsen <conner@launchbyte.io>
2019-12-12 22:02:10 -0800

    Add get user

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const fetch = require('node-fetch');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const token = "Bearer xoxp-457909895632-457909897568-873858915351-6b583d276546766dcebee91832ad9913";
+
 app.get('/', (req, res) => {
   console.log("Request received" + req.body);
   res.send("Success");
@@ -38,18 +40,32 @@ app.post('/corrupt', (req, res) => {
     method: "POST",
     headers: { 
       "Content-Type": "application/json",
-      Authorization: "Bearer xoxp-457909895632-457909897568-873858915351-6b583d276546766dcebee91832ad9913"
+      Authorization: 
     },
     body: JSON.stringify(answer)
   };
 
-  //fetch(req.body["response_url"], response)
   fetch("https://slack.com/api/chat.postMessage", response)
     .then((res) => {
       console.log("Code: " + res.json().statusCode);
     })
     .catch((err) => console.err(err));
 });
+
+function getUserInfo(userID) {
+  var prefix = "https://slack.com/api/users.info";
+  var url = `?token=${token}&user=${userID}`;
+
+  fetch(prefix + url)
+    .then((res) => {
+      console.log(res);
+      res.json();
+    })
+    .then((json) => {
+      return json;
+    })
+    .catch((err) => console.err(err));
+}
 
 app.listen(process.env.PORT, () => { console.log("Server started") });
 //app.listen(8081, () => { console.log("Server started") });
