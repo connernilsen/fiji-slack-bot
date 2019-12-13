55fc21cc0eb97de509ab1d3648d1299ec4aa4439
Conner Nilsen <conner@launchbyte.io>
2019-12-13 13:08:42 -0800

    Add expand text

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/corrupt', (req, res) => {
   res.send();
   var user = req.body["user_id"];
 
-  // get user info and send text as user
+  // get calling user's info and send response
   getUserInfo(user, (userJson) => {
     // set original text and intensity
     var text = req.body["text"];
@@ -65,38 +65,77 @@ app.post('/corrupt', (req, res) => {
   });
 });
 
+// expand specified text
 app.post('/expand', (req, res) => {
+  // send default response and get userID
   res.send();
   var user = req.body["user_id"];
 
+  // get calling user's info and send response
   getUserInfo(user, (userJson) => {
-    var text = req.body["text"].toUpperCase().trim();
+    var text = req.body["text"].trim();
     
+    // don't run if empty
     if (text.length == 0) {
       return;
     }
 
+    // function to expand string
     var expand = (str) => {
-      var arr = str.split("");
+      var arr = str.toUpperCase().split("");
       var ans = arr[0];
 
+      // loop through and append expanded letter
       for (i = 1; i < arr.length; i++) {
-        ans += ` ${arr[i]}`;
+        if (arr[i] != " ") {
+          ans += ` ${arr[i]}`;
+        }
       }
       return ans;
     }
 
-    var sp = text.split("/");
-    var ans = sp[0];
-    var tmp;
+    //initialize vars
+    var idx = text.indexOf("<");
+    var ans = "";
+    var tmp = "";
+    var end = -1;
+    
+    // loop through and greedily E X P A N D
+    while (idx != -1) {
+      // append non-selected text
+      ans += text.substring(0, idx);
 
-    for (i = 1; i < sp.length; i++) {
-      tmp = sp[i].split("\\");
-      ans += expand(tmp[0]);
-      ans += tmp[1];
+      // set end and expand through end
+      end = text.indexOf(">");
+      if (end != -1) {
+        tmp = text.substring(idx + 1, end);
+        ans += expand(tmp);
+      }
+      // if end not found, then expand rest of text
+      else {
+        tmp = text.substring(idx + 1);
+        ans += expand(tmp);
+      }
+
+      // set text to be the remaining text to check
+      if (end + 1 < text.length && end != -1) {
+        text = text.substring(end + 1);
+      }
+      // set to empty string if nothing left to expand
+      else {
+        text = "";
+      }
+
+      // get next selection
+      idx = text.indexOf("<");
     }
-    console.log(ans);
 
+    // append remaining text if there is any
+    if (text.length > 0) {
+      ans += text;
+    }
+
+    // prepare answer
     var answer = {
       text: "*" + ans + "*",
       channel: req.body["channel_id"],
