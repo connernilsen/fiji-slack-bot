a91a1f0543fafbd40b13b6e683c79f3d018fb843
Conner Nilsen <conner@launchbyte.io>
2019-12-12 20:31:50 -0800

    Negate if

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post('/corrupt', (req, res) => {
 
   var text = req.body["text"];
   var intensity = 0.8;
-  if (text.length > 1 && isNaN(parseInt(text.charAt(0)))) {
+  if (text.length > 1 && !isNaN(parseInt(text.charAt(0)))) {
     intensity = parseInt(text.charAt(0)) / 10;
     text = text.substring(1);
   }
